9bd0658363bc152734330d3fd8ea16d2a10bedac
Ndikrulil <developerroot9@gmail.com>
2024-10-12 10:01:59 +0700

    a

--- src/pages/resume/components/CtaButtons.jsx
+++ src/pages/resume/components/CtaButtons.jsx
@@ -38,28 +38,12 @@ export default function CtaButtons() {
     };
 
     return (
-        <div className='flex flex-col w-full justify-center gap-4 mt-10 xsm:flex-row'>
+        <div className='flex flex-col w-full justify-center gap-4 mt-10 xsm:flex-row bg-green-500'>
             <a href='https://api.whatsapp.com/send?phone=62895373847616' className='button'>
                 <PiEnvelopeSimple size='1.5rem' />
                 Whatsapp
             </a>
-            <button
-                onClick={handleDownload}
-                className={`button secondary flex items-center ${isDownloading ? 'bg-gray-500 cursor-not-allowed' : downloadSuccess ? 'bg-green-500' : 'bg-indigo-500'}`}
-                disabled={isDownloading}
-            >
-                {isDownloading ? (
-                    <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                    </svg>
-                ) : downloadSuccess ? (
-                    <PiCheck size='1.5rem' />
-                ) : (
-                    <PiDownloadSimple size='1.5rem' />
-                )}
-                {isDownloading ? 'Generating PDF...' : downloadSuccess ? 'Download Successful' : 'Download Resume'}
-            </button>
+
         </div>
     );
 }
