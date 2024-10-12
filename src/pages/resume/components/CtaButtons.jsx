4f567484ecd817a870a82820765deb119f3ddc6c
Ndikrulil <developerroot9@gmail.com>
2024-10-12 09:44:21 +0700

    a

--- src/pages/resume/components/CtaButtons.jsx
+++ src/pages/resume/components/CtaButtons.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { PiDownloadSimple, PiEnvelopeSimple, PiCheck } from "react-icons/pi";
 import fetchPDF from '../../../api/cvtopdf';
+import ArrowLink from '../../components/ui/ArrowLink';
 
 export default function CtaButtons() {
     const [isDownloading, setIsDownloading] = useState(false);
@@ -44,7 +45,23 @@ export default function CtaButtons() {
                 Email
             </a>
             <ArrowLink link='https://api.whatsapp.com/send?phone=62895373847616' anchorText='WA +62 8953-7384-7616' size='xl' className='mt-8' />
-            
+            <button
+                onClick={handleDownload}
+                className={`button secondary flex items-center ${isDownloading ? 'bg-gray-500 cursor-not-allowed' : downloadSuccess ? 'bg-green-500' : 'bg-indigo-500'}`}
+                disabled={isDownloading}
+            >
+                {isDownloading ? (
+                    <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg>
+                ) : downloadSuccess ? (
+                    <PiCheck size='1.5rem' />
+                ) : (
+                    <PiDownloadSimple size='1.5rem' />
+                )}
+                {isDownloading ? 'Generating PDF...' : downloadSuccess ? 'Download Successful' : 'Download Resume'}
+            </button>
         </div>
     );
 }
