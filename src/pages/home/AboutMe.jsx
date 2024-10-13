22efcfc748f572fd601f2ca8daa50ccb1fb4778b
Ndikrulil <developerroot9@gmail.com>
2024-10-13 16:24:48 +0700

    a

--- src/pages/home/AboutMe.jsx
+++ src/pages/home/AboutMe.jsx
@@ -29,7 +29,7 @@ function AboutMe() {
 Pengalaman saya membuat saya Insyaallah memenuhi syarat secara unik untuk inovasi dan pengembangan pemasaran digital melalui Google.
 Tujuan saya adalah selalu menciptakan solusi yang tidak hanya memenuhi tetapi juga melampaui harapan setiap klien saya.
                     </p>
-                    
+                    <CtaButtons />
                     <ArrowLink link='https://api.whatsapp.com/send?phone=62895373847616' anchorText='WA +62 8953-7384-7616' size='xl' className='mt-8' />
                 </div>
                 <div ref={ref2} className={`flex flex-col items-center lg:items-end lg:w-1/2 ${transitionClasses2}`}>
