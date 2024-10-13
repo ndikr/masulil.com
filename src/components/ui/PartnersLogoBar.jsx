ff6aadf8b4b03a29ab690e355a48232ecbfcde76
Ndikrulil <developerroot9@gmail.com>
2024-10-13 21:41:29 +0700

    a

--- src/components/ui/PartnersLogoBar.jsx
+++ src/components/ui/PartnersLogoBar.jsx
@@ -1,4 +1,4 @@
-import Dekra from '../../assets/gads.png';
+import Dekra from '../../assets/logo_google_googleads.svg';
 import Dtails from '../../assets/dtails-logo.svg';
 import Connexio from '../../assets/connexio-logo.svg';
 import KompetenceKanalen from '../../assets/kompetence-kanalen-logo.svg';
