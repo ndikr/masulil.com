6dba86ca2383df8ad8a7ca4a8cc6a7a1a02c501b
Ndikrulil <developerroot9@gmail.com>
2024-10-14 14:21:38 +0700

    a

--- src/pages/resume/Skills.jsx
+++ src/pages/resume/Skills.jsx
@@ -15,30 +15,23 @@ export default function Skills() {
             <div className='flex flex-col gap-6'>
                 <SkillGroup title={'Programming languages'}>
                     <Skill title={'Javascript'} />
-                    <Skill title={'TypeScript'} />
                     <Skill title={'Python'} />
-                    <Skill title={'Java'} />
                     <Skill title={'SQL'} />
-                    <Skill title={'GraphQL'} />
                     <Skill title={'HTML'} />
                     <Skill title={'CSS'} />
                 </SkillGroup>
 
-                <SkillGroup title={'Frameworks'}>
-                    <Skill title={'React'} />
-                    <Skill title={'Next'} />
-                    <Skill title={'Express'} />
-                    <Skill title={'Tailwindcss'} />
-                    <Skill title={'Django'} />
+                <SkillGroup title={'Platform'}>
+                    <Skill title={'Google Ads'} />
+                    <Skill title={'Facebook Ads'} />
+                    <Skill title={'Ahrefs'} />
+                    <Skill title={'Linux VPS'} />
+                    <Skill title={'Cloudflare'} />
                 </SkillGroup>
 
                 <SkillGroup title={'Others'}>
                     <Skill title={'Git'} />
                     <Skill title={'Google Cloud Platform'} />
-                    <Skill title={'Docker'} />
-                    <Skill title={'Scrum'} />
-                    <Skill title={'UML & ER Diagrams'} />
-                    <Skill title={'Flowcharts'} />
                     <Skill title={'Project Management'} />
                     <Skill title={'Entrepeneurship'} />
                     <Skill title={'Business Development'} />
