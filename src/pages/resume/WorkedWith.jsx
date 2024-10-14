b9e50e23775d293af5f6b7fa172a5f44ce74fcb6
Ndikrulil <developerroot9@gmail.com>
2024-10-14 13:06:42 +0700

    a

--- src/pages/resume/WorkedWith.jsx
+++ src/pages/resume/WorkedWith.jsx
@@ -5,8 +5,6 @@ import PartnersLogoBar from '../../components/ui/PartnersLogoBar';
 export default function WorkedWith() {
     return (
         <Container>
-//            <p className='text-center mb-6 text-base opacity-100'>Worked With</p>
-//            <Divider />
             <PartnersLogoBar />
         </Container>
     )
