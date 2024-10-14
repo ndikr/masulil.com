3081fa0671f46b8e7f4e99a9af2647431723420e
Ndikrulil <developerroot9@gmail.com>
2024-10-14 08:10:29 +0700

    a

--- src/pages/home/AboutMe.jsx
+++ src/pages/home/AboutMe.jsx
@@ -20,14 +20,15 @@ function AboutMe() {
         <Section id='about-me' className={`relative z-20`}>
             <div className='flex flex-col gap-12 lg:flex-row items-center'>
             <div ref={ref1} className={`flex flex-col lg:w-1/2 ${transitionClasses1}`}>
-                    <SectionTitle title='Tentang Mas Ulil' />
-                    <h2>Nikmati berbagai keuntungan beriklan di Google Adwords bersama Mas Ulil</h2>
+                    <h2>Keuntungan beriklan di Google Ads bersama Mas Ulil</h2>
                     <p>
                     <h4>Gratis Konsultasi</h4>
-                    </p> Mas Ulil memberikan konsultasi gratis bagi anda untuk memperoleh settingan terbaik untuk iklan Google Ads anda.
+                    Mas Ulil memberikan konsultasi gratis bagi anda untuk memperoleh settingan terbaik untuk iklan Google Ads anda.
+                    </p> 
                     <br></br>
                     <p>
-                    Pengalaman saya membuat saya Insyaallah memenuhi syarat secara unik untuk inovasi dan pengembangan pemasaran digital melalui Google.
+                    <h4>Riset Kata Kunci dan Kompetitor</h4>
+                    Kami menggunakan berbagai strategi untuk mendapatkan buying keyword tertarget hingga menggiring brand kompetitor untuk dijadikan target khusus.
                     </p>
                     <br></br>
                     <p>
