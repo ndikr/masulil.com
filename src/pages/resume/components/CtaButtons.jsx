d1f729859c397fbdd12ec2bbc8b2845804960fb8
Ndikrulil <developerroot9@gmail.com>
2024-10-16 21:15:45 +0700

    A

--- src/pages/resume/components/CtaButtons.jsx
+++ src/pages/resume/components/CtaButtons.jsx
@@ -39,7 +39,7 @@ export default function CtaButtons() {
 
     return (
         <div className='flex flex-col w-full justify-center gap-4 mt-10 xsm:flex-row'>
-            <a href='https://api.whatsapp.com/send?phone=62895373847616&text=Assalamualaikum%20mas%20Ulil,%20Saya%20berminat%20menggunakan%20jasa%20Google%20Ads%20anda,%20bisa%20tolong%20dibantu?' className='button'>
+            <a href='https://api.whatsapp.com/send?phone=62882005295050&text=Assalamualaikum%20mas%20Ulil,%20Saya%20berminat%20menggunakan%20jasa%20Google%20Ads%20anda,%20bisa%20tolong%20dibantu?' className='button'>
                 <PiEnvelopeSimple size='1.5rem' />
                 Whatsapp
             </a>
