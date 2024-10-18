5bcc565f6bc62548d2d4a590a81a09dabdc44f66
ndikrulil <developerroot9@gmail.com>
2024-10-18 10:00:48 +0700

    a

--- src/pages/portfolio/project/index.jsx
+++ src/pages/portfolio/project/index.jsx
@@ -11,7 +11,7 @@ import ScrollButton from '../../../components/ui/ScrollButton';
 
 const ProjectPage = () => {
   const { projectId } = useParams();
-  const project = projectsData.find(p => p.id === self);
+  const project = projectsData.find(p => p.id === projectId);
 
   if (!project) {
     return <NotFound />;
