d1257b251324de7b8404a0606bdfbb1dff1646e9
ndikrulil <developerroot9@gmail.com>
2024-11-20 06:45:33 +0700

    a

--- src/components/layout/Section.jsx
+++ src/components/layout/Section.jsx
@@ -1,16 +1,13 @@
-import Container from './Container';
+import Container from './Container'
 
 const Section = ({ children, className, id }) => {
     return (
-        <section
-            id={id}
-            className={`py-20 md:py-40 flex items-center odd:bg-black-primary even:bg-black-secondary ${className}`}
-        >
-            <Container style={{ paddingBottom: '20px' }}>
+        <section id={id} className={`py-20 md:py-40 flex items-center odd:bg-black-primary even:bg-black-secondary ${className}`}>
+            <Container>
                 {children}
             </Container>
         </section>
-    );
-};
+    )
+}
 
-export default Section;
+export default Section
